e6278cd1079a753415b3ec94cf68659428eaeceb
Tobias Scholze <Tobias@Apfeltasche.fritz.box>
2015-07-12 14:10:18 +0200

    added first json interface api/all to the app

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^api/all/$', views.quotes_as_json, name='all_json'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from django.core import serializers
 
 from .models import Quote
 
@@ -9,4 +10,8 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_quote_list'
 	def get_queryset(self):
 		"""Return the ten latest quotes"""
-		return Quote.objects.order_by('-pub_date')[:10] 
+		return Quote.objects.order_by('-pub_date')[:10]
+
+def quotes_as_json(self):
+	data = serializers.serialize("json", Quote.objects.order_by('-pub_date')[:10])
+	return HttpResponse(data, content_type='application/json')	 
