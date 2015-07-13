b98c670a3801a9a2d8aecf3f9906fcfe2911099b
Tobias Scholze <Tobias@Apfeltasche.fritz.box>
2015-07-13 19:07:07 +0200

    Added latest-quote rss view; added detail page

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^api/all/$', views.quotes_as_json, name='all_json'),
+    url(r'^feed/all/$', views.LatestQuotesFeed(), name='all_feed'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from django.core import serializers
+from django.core.urlresolvers import reverse
+from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Atom1Feed
 
 from .models import Quote
 
@@ -11,7 +14,29 @@ class IndexView(generic.ListView):
 	def get_queryset(self):
 		"""Return the ten latest quotes"""
 		return Quote.objects.order_by('-pub_date')[:10]
+		
+class DetailView(generic.DetailView):
+	model = Quote
+	template_name = "quotes/detail.html"
 
+class LatestQuotesFeed(Feed):
+	feed_type = Atom1Feed
+	title = "-- Quotes --"
+	link = "/sitenews/"
+	description = "Do not forget unique spoken words"
+	
+	def items(self):
+		return Quote.objects.order_by('-pub_date')[:10]
+	
+	def item_title(self, item):
+		return item.quote_text
+
+	def item_description(self, item):
+		return item.author_name
+		
+	def item_link(self, item):
+		return reverse('quotes:detail', args=(item.pk,))
+	
 def quotes_as_json(self):
 	data = serializers.serialize("json", Quote.objects.order_by('-pub_date')[:10])
 	return HttpResponse(data, content_type='application/json')	 
